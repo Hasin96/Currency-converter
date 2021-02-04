795b7a1fb2e25e51309724f81c69b4c74f55b02f
Hasin Ajaz <hasin.ajaz1996@hotmail.com>
2021-02-04 04:35:56 +0000

    Added a franc object copy and pasted from Dollar

---
+++ MultiCurrencyMoney.UnitTests/FrancUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiCurrencyMoney.UnitTests
+{
+    [TestFixture]
+    class FrancUnitTests
+    {
+        public FrancUnitTests()
+        {
+
+        }
+
+        [TestCase(2, 10)]
+        [TestCase(3, 15)]
+        public void Times_WhenCalledWithMultiplier_ReturnsNewFrancObjectWithTheExpectedAmount(int multiplier, int expectedAmount)
+        {
+            Franc five = new Franc(5);
+
+            Franc product = five.Times(multiplier);
+
+            Assert.AreEqual(product, new Franc(expectedAmount));
+        }
+    }
+}
---
+++ MultiCurrencyMoney/Franc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiCurrencyMoney
+{
+    public class Franc
+    {
+        private int _amount;
+        public Franc(int amount)
+        {
+            _amount = amount;
+        }
+
+        public Franc Times(int multiplier)
+        {
+            return new Franc(_amount * multiplier);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (this.GetType() != obj.GetType())
+                return false;
+
+            Franc Franc = (Franc)obj;
+            if (this._amount == Franc._amount)
+                return true;
+            else
+                return false;
+        }
+    }
+}
