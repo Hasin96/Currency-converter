2c0528102c7bc50a8a85992f1dcd152194562e06
Hasin Ajaz <hasin.ajaz1996@hotmail.com>
2021-02-04 09:07:44 +0000

    Deleted sub classes and made common methods inside the money base class

---
+++ MultiCurrencyMoney.UnitTests/MoneyUnitTests.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiCurrencyMoney.UnitTests
+{
+    [TestFixture]
+    public class MoneyUnitTests
+    {
+        public MoneyUnitTests()
+        {
+
+        }
+
+        [Test]
+        public void Currency_CorrectCurrency_ReturnsCorrectCurrency()
+        {
+            StringAssert.Contains("USD", Money.Dollar(1).Currency());
+        }
+
+        [TestCase(2, 10)]
+        [TestCase(3, 15)]
+        public void Times_WhenCalledWithMultiplier_ReturnsNewFrancObjectWithTheExpectedAmount(int multiplier, int expectedAmount)
+        {
+            Money five = Money.Franc(5);
+
+            Money product = five.Times(multiplier);
+
+            Assert.AreEqual(product, Money.Franc(expectedAmount));
+        }
+
+        [TestCase(2, 10)]
+        [TestCase(3, 15)]
+        public void Times_WhenCalledWithMultiplier_ReturnsNewDollarObjectWithTheExpectedAmount(int multiplier, int expectedAmount)
+        {
+            Money five = Money.Dollar(5);
+
+            Money product = five.Times(multiplier);
+
+            Assert.AreEqual(product, Money.Dollar(expectedAmount));
+        }
+
+        [TestCase(5, 5, true)]
+        [TestCase(5, 6, false)]
+        public void Equals_CompareTwoDollarObjects_ReturnsExpected(int firstDollarAmount, int secondDollarAmount, bool expectedResult)
+        {
+            Money five1 = Money.Dollar(firstDollarAmount);
+            Money five2 = Money.Dollar(secondDollarAmount);
+
+            Assert.AreEqual(five1.Equals(five2), expectedResult);
+        }
+
+        [Test]
+        public void Equals_CompareDollarToNull_ReturnsFalse()
+        {
+            Money five = Money.Dollar(5);
+            object nullObj = null;
+
+            Assert.IsFalse(five.Equals(nullObj));
+        }
+
+        public void Equals_CompareDollarToFrancObjects_ReturnsFalse()
+        {
+            Money five1 = Money.Dollar(5);
+            Money five2 = Money.Franc(5);
+
+            Assert.IsFalse(five1.Equals(five2));
+        }
+    }
+}
--- MultiCurrencyMoney/Money.cs
+++ MultiCurrencyMoney/Money.cs
@@ -7,20 +7,39 @@ namespace MultiCurrencyMoney
     public class Money 
     {
         protected int _amount;
+        protected string _currency;
+
+        public Money(int amount, string currency)
+        {
+            _amount = amount;
+            _currency = currency;
+        }
+
+        public string Currency() => _currency;
 
         public override bool Equals(object obj)
         {
             if (obj == null)
                 return false;
 
-            if (this.GetType() != obj.GetType())
-                return false;
-
             Money money = (Money)obj;
-            if (this._amount == money._amount)
-                return true;
-            else
-                return false;
+
+            return this._amount == money._amount && Currency().Equals(money.Currency());
+        }
+
+        public static Money Dollar(int value)
+        {
+            return new Money(value, "USD");
+        }
+
+        public static Money Franc(int value)
+        {
+            return new Money(value, "CHF");
+        }
+
+        public Money Times(int multiplier)
+        {
+            return new Money(_amount * multiplier, _currency);
         }
     }
 }
